5d013ec158c5f07e66b3f4ef75cc9e1f743b188f
Myint Myat Hein <myintmyathein.mmh1996@gmail.com>
2023-01-05 20:47:43 +0630

    product edit fix

--- resources/views/dashboard/products/edit.blade.php
+++ resources/views/dashboard/products/edit.blade.php
@@ -66,106 +66,129 @@
                                         </span>
                                     @enderror
                                 </div>
-                                <div class="col-md-6 mb-3">
-                                    <label for="quantity" style="font-weight: 700">Gem Quantity:</label>
-                                    <input type="text" name="quantity"
-                                        class="@error('quantity') is-invalid @enderror form-control py-1" required
-                                        value="{{ old("quantity",$product->quantity) }}">
-                                    @error('quantity')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                                <div class="col-md-6 mb-3">
-                                    <label for="weight" style="font-weight: 700">Gem weight:</label>
-                                    <input type="text" name="weight"
-                                        class="@error('weight') is-invalid @enderror form-control py-1" required
-                                        value="{{ old("weight",$product->weight) }}">
-                                    @error('weight')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                                <div class="col-md-6 mb-3">
-                                    <label for="price" style="font-weight: 700">Gem price:</label>
-                                    <input type="text" name="price"
-                                        class="@error('price') is-invalid @enderror form-control py-1" required
-                                        value="{{ old("price",$product->price) }}">
-                                    @error('price')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                                <div class="col-md-6 mb-3">
-                                    <label for="gold_quantity" style="font-weight: 700">Gold Quantity:</label>
-                                    <input type="text" name="gold_quantity"
-                                        class="@error('gold_quantity') is-invalid @enderror form-control py-1" required
-                                        value="{{ old("gold_quantity",$product->gold_quantity) }}">
-                                    @error('gold_quantity')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                                <div class="col-md-6 mb-3">
-                                    <label for="gold_price" style="font-weight: 700">Gold Price:</label>
-                                    <input type="text" name="gold_price"
-                                        class="@error('gold_price') is-invalid @enderror form-control py-1" required
-                                        value="{{ old("gold_price",$product->gold_price) }}">
-                                    @error('gold_price')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                                <div class="col-md-6 mb-3">
-                                    <label for="ad_gold_quantity" style="font-weight: 700">AD Gold Quantity:</label>
-                                    <input type="text" name="ad_gold_quantity"
-                                        class="@error('ad_gold_quantity') is-invalid @enderror form-control py-1" required
-                                        value="{{ old("ad_gold_quantity",$product->ad_gold_quantity) }}">
-                                    @error('ad_gold_quantity')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                                <div class="col-md-6 mb-3">
-                                    <label for="ad_gold_price" style="font-weight: 700">AD Gold Price:</label>
-                                    <input type="text" name="ad_gold_price"
-                                        class="@error('ad_gold_price') is-invalid @enderror form-control py-1" required
-                                        value="{{ old("ad_gold_price",$product->ad_gold_price) }}">
-                                    @error('ad_gold_price')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                                <div class="col-md-6 mb-3">
-                                    <label for="service_charges" style="font-weight: 700">Service Charges:</label>
-                                    <input type="text" name="service_charges"
-                                        class="@error('service_charges') is-invalid @enderror form-control py-1" required
-                                        value="{{ old("service_charges",$product->service_charges) }}">
-                                    @error('service_charges')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                                <div class="col-md-6 mb-3">
-                                    <label for="total_price" style="font-weight: 700">Total Price:</label>
-                                    <input type="text" name="total_price"
-                                        class="@error('total_price') is-invalid @enderror form-control py-1" required
-                                        value="{{ old("total_price",$product->total_price) }}">
-                                    @error('total_price')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
+                                
+                            <div class="col-md-6 mb-3">
+                                <label for="quantity" style="font-weight: 700">Gem Quantity:</label>
+                                <input type="text" name="quantity" id="quantity"
+                                    class="@error('quantity') is-invalid @enderror form-control py-1" required
+                                    value="{{ old('quantity',$product->quantity) }}">
+                                @error('quantity')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
+                            <div class="col-md-6 mb-3">
+                                <label for="weight" style="font-weight: 700">Gem weight:</label>
+                                <input type="text" name="weight" id="weight"
+                                    class="@error('weight') is-invalid @enderror form-control py-1" required
+                                    value="{{ old('weight',$product->weight) }}">
+                                @error('weight')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="col-md-6 mb-3">
+                                <label for="price" style="font-weight: 700">Gem price:</label>
+                                <input type="text" name="price" id="price"
+                                    class="@error('price') is-invalid @enderror form-control py-1" required
+                                    value="{{ old('price',$product->price) }}">
+                                @error('price')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="col-md-3 mb-3">
+                                <label for="gold_quantity_p" style="font-weight: 700">Gold Quantity(P):</label>
+                                <input type="text" name="gold_quantity_p" id="gold_quantity_p"
+                                    class="@error('gold_quantity_p') is-invalid @enderror form-control py-1" required
+                                    value="{{ old('gold_quantity_p',$product->gold_quantity_p) }}">
+                                @error('gold_quantity_p')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="col-md-3 mb-3">
+                                <label for="gold_quantity_y" style="font-weight: 700">Gold Quantity(Y):</label>
+                                <input type="text" name="gold_quantity_y" id="gold_quantity_y"
+                                    class="@error('gold_quantity_y') is-invalid @enderror form-control py-1" required
+                                    value="{{ old('gold_quantity_y',$product->gold_quantity_y) }}">
+                                @error('gold_quantity_y')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="col-md-6 mb-3">
+                                <label for="gold_price" style="font-weight: 700">Gold Price:</label>
+                                <input type="text" name="gold_price" id="gold_price"
+                                    class="@error('gold_price') is-invalid @enderror form-control py-1" required
+                                    value="{{ old('gold_price',$product->gold_price) }}">
+                                @error('gold_price')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="col-md-3 mb-3">
+                                <label for="ad_gold_quantity_p" style="font-weight: 700">AD Gold Quantity(P):</label>
+                                <input type="text" name="ad_gold_quantity_p" id="ad_gold_quantity_p"
+                                    class="@error('ad_gold_quantity_p') is-invalid @enderror form-control py-1" required
+                                    value="{{ old('ad_gold_quantity_p',$product->ad_gold_quantity_p) }}">
+                                @error('ad_gold_quantity_p')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="col-md-3 mb-3">
+                                <label for="ad_gold_quantity_y" style="font-weight: 700">AD Gold Quantity(Y):</label>
+                                <input type="text" name="ad_gold_quantity_y" id="ad_gold_quantity_y"
+                                    class="@error('ad_gold_quantity_y') is-invalid @enderror form-control py-1" required
+                                    value="{{ old('ad_gold_quantity_y',$product->ad_gold_quantity_y) }}">
+                                @error('ad_gold_quantity_y')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="col-md-6 mb-3">
+                                <label for="ad_gold_price" style="font-weight: 700">AD Gold Price:</label>
+                                <input type="text" name="ad_gold_price" id="ad_gold_price"
+                                    class="@error('ad_gold_price') is-invalid @enderror form-control py-1" required
+                                    value="{{ old('ad_gold_price',$product->ad_gold_price) }}">
+                                @error('ad_gold_price')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="col-md-6 mb-3">
+                                <label for="service_charges" style="font-weight: 700">Service Charges:</label>
+                                <input type="text" name="service_charges" id="service_charges"
+                                    class="@error('service_charges') is-invalid @enderror form-control py-1" required
+                                    value="{{ old('service_charges',$product->service_charges) }}">
+                                @error('service_charges')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="col-md-6 mb-3">
+                                <label for="total_price" style="font-weight: 700">Total Price:</label>
+                                <input type="text" name="total_price" id="total_price"
+                                    class="@error('total_price') is-invalid @enderror form-control py-1" required
+                                    value="{{ old('total_price',$product->total_price) }}">
+                                @error('total_price')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
 
                             <div class="row my-3">
                                 <div class="d-flex justify-content-end">
