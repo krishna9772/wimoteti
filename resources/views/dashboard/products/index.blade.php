d72c93f6190b290830b4b24e4b9c23a1f3a3b74a
santoshgtm95 <santosh.gtm6@gmail.com>
2023-02-22 08:04:01 +0630

    [commit] image click model popup

--- resources/views/dashboard/products/index.blade.php
+++ resources/views/dashboard/products/index.blade.php
@@ -1,6 +1,64 @@
 @extends('layouts.app')
 @section('title', 'Product')
 @section('content')
+<style>
+    /* The Modal (background) */
+#myModal1 {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  padding-top: 100px; /* Location of the box */
+  left: 0;
+  top: 60px;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */ 
+  background-color: rgba(90,90,85,0.9); /* Black w/ opacity */
+}
+
+/* Modal Content (image) */
+#modal-content {
+  margin: auto;
+  display: block;
+  width: 80%;
+  max-width: 700px;
+}
+ 
+@-webkit-keyframes zoom {
+  from {-webkit-transform:scale(0)} 
+  to {-webkit-transform:scale(1)}
+}
+
+@keyframes zoom {
+  from {transform:scale(0)} 
+  to {transform:scale(1)}
+}
+
+/* The Close Button */
+.close {
+  position: absolute;
+  top: 15px;
+  right: 35px;
+  color: #bbb;
+  font-size: 40px;
+  font-weight: bold;
+  
+}
+
+.close:hover,
+.close:focus {
+  color: #bbb;
+  text-decoration: none;
+  cursor: pointer;
+}
+
+/* 100% Image Width on Smaller Screens */
+@media only screen and (max-width: 700px){
+  .modal-content {
+    width: 100%;
+  }
+}
+</style>
     <div class="d-flex align-items-center justify-content-between">
         <div class="pagetitle">
             <h1>Product Page</h1>
@@ -49,7 +107,7 @@
                                             </td> --}}
                                             
                                             <td><img src="{{ asset("storage/".$product->image) }}"
-                                                alt="" width="60px" height="60px">
+                                                alt="" width="60px" height="60px" onclick="clickImage(this)">
                                             </td>
                                             <td>{{ $product->code }}</td>
                                             <td>{{ $product->getcategory->name }}</td>
@@ -87,9 +145,14 @@
 
                     </div>
                 </div>
-
+           
             </div>
         </div>
+        <div id="myModal1" class="modal">
+            <span class="close" id="close">&times;</span>
+            <img class="modal-content" id="modal-content">
+           
+          </div>
     </section>
 @endsection
 @section('script')
@@ -116,6 +179,20 @@
                 })
             });
         });
+        function clickImage(img) {
+     
+     var modal = document.getElementById("myModal1");  
+     var modalImg = document.getElementById("modal-content");
+     modal.style.display = "block";
+     modalImg.src = img.src;
+    
+ }
+ var span = document.getElementById('close');
+    span.onclick = function() { 
+        $('#myModal1').css({'display': 'none'});
+
+    };
+
     </script>
 
 @endsection
