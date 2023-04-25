96a13eb2a8ec879ad1d9d184095e4c597653ea83
Aung Naing Thu <aryalkrishna642@gmail.com>
2023-04-25 16:16:51 +0630

    Total amount integrated in report section

--- app/Http/Controllers/Dashboard/ReportController.php
+++ app/Http/Controllers/Dashboard/ReportController.php
@@ -10,19 +10,22 @@ class ReportController extends Controller
 {
     public function index(Request $request){
 
-        $product_in = Product::select('id','code','price','total_price')
+        $product_in = Product::select('id','code','price','total_price')->where("product_in",1)
         ->where("status",1)->paginate(3);
         $product_out = Product::select('id','code','price','total_price')
         ->where("status",1)->where("product_in",0)->paginate(3);
+        $product_in_total_price = Product::where('status',1)->where('product_in',1)->sum('total_price');
+        $product_out_total_price = Product::where('status',1)->where('product_in',0)->sum('total_price');
+
         
-        return view('dashboard.report.index',compact("product_in","product_out"));
+        return view('dashboard.report.index',compact("product_in","product_out",'product_in_total_price','product_out_total_price'));
     }
 
     public function filter(Request $request){
 
 
         $product_in = Product::select('id','code','price')
-        ->where("status",1)
+        ->where("status",1)->where("product_in",1)
         ->whereBetween('created_at',[$request->from_date, $request->to_date])
         ->paginate(3);
 
@@ -30,7 +33,11 @@ class ReportController extends Controller
         ->where("status",1)->where("product_in",0)
         ->whereBetween('created_at',[$request->from_date, $request->to_date])
         ->paginate(3);
+
+        $product_in_total_price = Product::where('status',1)->where('product_in',1)->sum('total_price');
+        $product_out_total_price = Product::where('status',1)->where('product_in',0)->sum('total_price');
+
          
-        return view('dashboard.report.index',compact("product_in","product_out"));
+        return view('dashboard.report.index',compact("product_in","product_out",'product_in_total_price','product_out_total_price'));
     }
 }
